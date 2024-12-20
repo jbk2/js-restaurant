5aa7a4f74be271f1ed7e5b97f951964166034bf1
jbk2 <james@humanbenefits.com>
2024-12-20 17:12:38 +0100

    Build homepage content via JS

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,77 @@
+import sushiImage from './assets/sushi1.svg';
+import pet1Image from './assets/pet1.webp';
+import pet2Image from './assets/pet2.jpg';
+import pet3Image from './assets/pet3.webp';
+
 function renderHomePage() {
   console.log('connected ok');
+  document.body.append(ingredientsSection(), chefSection(), reviewsSection());
 }
+
+const ingredientsSection = () => {
+  let section = document.createElement('section');
+  section.id = "ingredients-section";
+  section.innerHTML = `
+    <div>
+      <h1>Fine quality, fresh ingredients</h1>
+      <div class="content">
+        <img id="sushi1-svg" src="${sushiImage}" alt="some sushi">
+        <p>Best quality line caught sushi SVGs, from sustainable local designers.</p>
+      </div>
+    </div>
+  `;
+  return section;
+};
+
+const chefSection = () => {
+  let section = document.createElement('section');
+  section.id = "chef-section";
+  section.innerHTML = `
+    <h1>Chef Banzai</h1>
+    <p>Trained under Japanese sushi masters, since the age of 5 and for 72 years, Chef Banzai Sarumaku
+      knows well how to fillet a fish by now. True to his family's Samurai heritage his filleting
+      method is with sword, often accompanied by his notorious shreiking of famous samurai battle cries.
+    </p>
+  `;
+  return section;
+};
+
+const reviewsSection = () => {
+  let section = document.createElement('section');
+  section.id = "reviews-section";
+  section.innerHTML = `
+    <h1>Reviews</h1>
+    <div id="reviews">
+      <div id="review-1">
+        <div class="image-mask">
+          <img id="pet1" src="${pet1Image}" alt="aristocratic cat">
+        </div>
+        <p>
+          <span>Millicent Houghton-Bogtrotter ('Boggie') - </span>
+          "Marvelous, a dining experience quite unlike any other."
+        </p>
+      </div>
+      <div id="review-2">
+        <div class="image-mask">
+          <img id="pet2" src="${pet2Image}" alt="aristocratic dog">
+        </div>
+        <p>
+          <span>Cecil Chadwick-Smotherington ('Chadders') - </span>
+          "Frightfully foreign stuff, one ought be accompanied by Primus."
+        </p>
+      </div>
+      <div id="review-3">
+        <div class="image-mask">
+          <img id="pet3" src="${pet3Image}" alt="aristocratic labrador">
+        </div>
+        <p>
+          <span>Humphrey Headingly-Snodsbottom ('Snodders') - </span>
+          "Rather agreeable, though bracing, oriental grub. Import to Whites perhaps."
+        </p>
+      </div>
+    </div>
+  `;
+  return section;
+};
 
 export { renderHomePage };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './assets/styles.css'
 import { renderHomePage } from "./homepage.js"
 
-console.log("Hello World!");
-
 document.addEventListener('DOMContentLoaded', () => {
   renderHomePage();
 })
