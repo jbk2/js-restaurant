58caac5cfdd24745d6a3aca1721d174c966628b2
jbk2 <james@humanbenefits.com>
2024-12-22 20:58:50 +0100

    Header styling and stylesheet organisation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './assets/styles.css'
-import './assets/menu_page_styles.css'
-import './assets/contact_page_styles.css'
+import './assets/stylesheets/styles.css'
+import './assets/stylesheets/home_page_styles.css'
+import './assets/stylesheets/menu_page_styles.css'
+import './assets/stylesheets/contact_page_styles.css'
 import { renderHomePage } from "./homepage.js" // used a named export, thus parenthses
 import renderMenuPage from "./menupage.js" // used export default here thus no parenteses here
 import renderContactPage from "./contactpage.js" // used export default here thus no parenteses here
@@ -9,11 +10,17 @@ function clearContent() {
   document.getElementById('content').innerHTML = '';
 }
 
-function triggerAnimation() {
-  const logo = document.querySelector('#logo-type');
+function triggerAnimations() {
+  const logo = document.getElementById('logo-type');
+  const flag = document.getElementById('japanese-flag');
+  console.log(logo);
+  console.log(flag);
   logo.classList.remove('animate-logo');
+  flag.classList.remove('animate-flag');
   void logo.offsetWidth;
-  logo.classList.add('animate-logo'); // Reapply the animation class
+  void flag.offsetWidth;
+  logo.classList.add('animate-logo');
+  flag.classList.add('animate-flag'); // Reapply the animation class
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -32,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.addEventListener('click', function () {
       const functionName = `render${this.textContent}Page`;
       clearContent();
-      triggerAnimation();
+      triggerAnimations();
       renderFunctions[functionName]();
     })
   });
